97ea1cbc5d9eb7f847ea25e5504f8c487fc4037d
Mrgaton <arnaupost4@gmail.com>
2024-02-28 20:08:49 +0100

    Hided console

--- Estufa simulator/Program.cs
+++ Estufa simulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -9,10 +10,13 @@ namespace Estufa_simulator
     internal static class Program
     {
         private static SHA512 hashAlgorithm = SHA512Managed.Create();
+
         private static byte[] buffer = new byte[int.MaxValue / 2];
 
         private static void Main(string[] args)
         {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.Idle;
+
             Task.Factory.StartNew(() =>
             {
                 Parallel.ForEach(IterateUntilFalse(() => true), new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount }, async ignored =>
@@ -21,7 +25,7 @@ namespace Estufa_simulator
                 });
             });
 
-            MessageBox.Show(":D | Estufa simulator iniciada, presione ok para apagar la estufa", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Estufa simulator iniciada, presione ok para apagar la estufa", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
         private static IEnumerable<bool> IterateUntilFalse(Func<bool> condition)
