e133e9220d811c7b8b759b90580917b624b1c8c0
Mrgaton <arnaupost4@gmail.com>
2024-02-28 20:06:18 +0100

    Agregar archivos de proyecto.

---
+++ Estufa simulator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estufa_simulator
+{
+    internal static class Program
+    {
+        private static SHA512 hashAlgorithm = SHA512Managed.Create();
+        private static byte[] buffer = new byte[int.MaxValue / 2];
+
+        private static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                Parallel.ForEach(IterateUntilFalse(() => true), new ParallelOptions { MaxDegreeOfParallelism = Environment.ProcessorCount }, async ignored =>
+                {
+                    while (true) hashAlgorithm.ComputeHash(buffer);
+                });
+            });
+
+            MessageBox.Show(":D | Estufa simulator iniciada, presione ok para apagar la estufa", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private static IEnumerable<bool> IterateUntilFalse(Func<bool> condition)
+        {
+            while (condition()) yield return true;
+        }
+    }
+}
---
+++ Estufa simulator/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+// La información general de un ensamblado se controla mediante el siguiente
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Estufa simulator")]
+[assembly: AssemblyDescription("La increíble y inigualable estufa simulator")]
+[assembly: AssemblyProduct("Estufa simulator")]
+[assembly: AssemblyCopyright("Copyright mrgaton©  2024")]
